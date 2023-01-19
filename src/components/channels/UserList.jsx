6df97afd37c89a25a00aa826c796034c344c1f5f
pigfyy <111782362+pigfyy@users.noreply.github.com>
2023-01-19 12:36:35 -0800

    made progress on importing channel and conversations

--- src/components/channels/UserList.jsx
+++ src/components/channels/UserList.jsx
@@ -1,6 +1,46 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import { db, auth } from "../../firebase.js";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { collection, query, onSnapshot, doc, getDoc } from "firebase/firestore";
 
 function UserList() {
+  const [conversationIds, setConversationIds] = useState([]);
+  const [conversations, setConversations] = useState([]);
+  const [user] = useAuthState(auth);
+
+  useEffect(() => {
+    const q1 = query(collection(db, `users/${user.uid}/conversations`));
+    const conversationIds = [];
+    const unsubscribe1 = onSnapshot(q1, (querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        conversationIds.push(doc.id);
+      });
+    });
+
+    // const q2 = query(collection(db, "conversations"));
+    // const conversations = [];
+    // const unsubscribe2 = onSnapshot(q2, (querySnapshot) => {
+    //   querySnapshot.forEach((doc) => {
+    //     conversations.push({ ...doc.data(), id: doc.id });
+    //   });
+    // });
+
+    setConversationIds(conversationIds);
+    // setConversations(conversations);
+
+    return () => {
+      unsubscribe1();
+      // unsubscribe2();
+    };
+  }, []);
+
+  useEffect(() => {
+    conversationIds.forEach((id) => {
+      console.log(id);
+    });
+  }, [conversationIds]);
+
   const list = [
     {
       name: "All Chat",
