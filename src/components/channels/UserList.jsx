6db3320e8b9c30397bc8305dccdeb6ed8b64dd51
pigfyy <111782362+pigfyy@users.noreply.github.com>
2023-01-20 18:14:35 -0800

    worked on userlist

--- src/components/channels/UserList.jsx
+++ src/components/channels/UserList.jsx
@@ -12,67 +12,67 @@ function UserList() {
   useEffect(() => {
     const q1 = query(collection(db, `users/${user.uid}/conversations`));
     const conversationIds = [];
-    const unsubscribe1 = onSnapshot(q1, (querySnapshot) => {
+    const unsubscribe = onSnapshot(q1, (querySnapshot) => {
       querySnapshot.forEach((doc) => {
         conversationIds.push(doc.id);
       });
     });
 
-    // const q2 = query(collection(db, "conversations"));
-    // const conversations = [];
-    // const unsubscribe2 = onSnapshot(q2, (querySnapshot) => {
-    //   querySnapshot.forEach((doc) => {
-    //     conversations.push({ ...doc.data(), id: doc.id });
-    //   });
-    // });
-
     setConversationIds(conversationIds);
-    // setConversations(conversations);
 
     return () => {
-      unsubscribe1();
-      // unsubscribe2();
+      unsubscribe();
     };
   }, []);
 
   useEffect(() => {
-    conversationIds.forEach((id) => {
-      console.log(id);
+    const promises = conversationIds.map((id) => {
+      id = id.replace(/\s/g, "");
+      const getConversation = async () => {
+        const docRef = doc(db, `conversations/${id}`);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+          return docSnap.data();
+        } else {
+          console.log(`No such conversation! Conversation ID: ${id}`);
+          return null;
+        }
+      };
+      return getConversation();
     });
+
+    Promise.all(promises)
+      .then((conversations) => {
+        setConversations(conversations.filter((c) => c !== null));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    return () => {
+      setConversations([]);
+    };
   }, [conversationIds]);
 
-  const list = [
-    {
-      name: "All Chat",
-      pfp: "https://firebasestorage.googleapis.com/v0/b/discord-clone-cae29.appspot.com/o/groupChatIcons%2Fblue.png?alt=media&token=a8dd8c96-b4a6-438e-b5c4-3a931561c707",
-      isSelected: true,
-    },
-    {
-      name: "User 1",
-      pfp: "https://firebasestorage.googleapis.com/v0/b/discord-clone-cae29.appspot.com/o/characterPfps%2F1.png?alt=media&token=6f6ceb75-e952-41c9-9902-5592fdbbc321",
-      isSelected: false,
-    },
-  ];
-
-  const users = list.map((user) => {
+  const users = conversations.map((conversation) => {
     return (
       <li className="px-1.5" key={crypto.randomUUID()}>
         <button
           className={`flex w-full items-center gap-[14px] rounded-[4px] p-1.5 ${
-            user.isSelected ? "bg-neutral-600" : "group hover:bg-neutral-600"
+            false ? "bg-neutral-600" : "group hover:bg-neutral-600"
           }`}
         >
           <div className="h-8 w-8 overflow-hidden rounded-full">
-            <img src={user.pfp} alt="" />
+            <img src={conversation.groupPfp} alt="" />
           </div>
           <span
             className={`text-base font-medium leading-5 ${
-              user.isSelected
+              false
                 ? "text-neutral-100"
                 : "text-neutral-300 group-hover:text-neutral-150"
             }`}
           >
-            {user.name}
+            {conversation.groupName}
           </span>
         </button>
       </li>
